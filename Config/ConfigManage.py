db49531e919288a8b1ca5aabbd937a36bd4b6926
xiaofeige <1149151981@qq.com>
2016-12-13 00:06:04 +0800

    初始化

---
+++ Config/ConfigManage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: ??
+@author: luffyren
+@site: http://www.luffyren.club
+@software: PyCharm Community Edition
+@file: ConfigManage.py
+@time: 2016/12/13 0:00
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
+
