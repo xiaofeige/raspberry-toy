bdf1e953f5b405999888e0ce330a8a51d6df41bc
小飞 <1149151981@qq.com>
2016-12-15 11:36:51 +0800

    add email surpport

---
+++ RaspMail/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: v1.0
+@author: fayren
+@site: http://www.luffyren.club
+@software: PyCharm Community Edition
+@file: RaspberryClient.py
+@time: 2016/12/12 23:52
+"""
