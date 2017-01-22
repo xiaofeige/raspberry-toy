87a8d9d088a4347fec58275abcdcaa7d174ffc71
xiaofeige <1149151981@qq.com>
2017-01-22 17:19:22 +0800

    new file

---
+++ sound/RaspSound.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: ??
+@author: luffyren
+@site: http://www.luffyren.club
+@software: PyCharm Community Edition
+@file: sound.py
+@time: 2017/1/22 17:10
+"""
+
+class RaspSound():
+    def __init__(self):
+        pass
+
+    def play_music(self):
+        pass
+
+    def say(self, msg):
+        pass
+
+
+if __name__ == '__main__':
+    pass
+
