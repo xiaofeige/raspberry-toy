8fc1681d64bd9fe150c9e00b6e990c3fe9dc780b
xiaofeige <1149151981@qq.com>
2017-02-17 14:40:01 +0800

    add message about

---
+++ network/packet.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: ??
+@author: luffyren
+@site: http://www.luffyren.club
+@software: PyCharm Community Edition
+@file: packet.py
+@time: 2017/2/17 14:37
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
+
