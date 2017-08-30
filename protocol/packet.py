b34356a95f6668d5b02f2dd634d5a6832d540e9c
xaiofeige <1149151981@qq.com>
2017-08-30 19:57:54 +0800

    delete network && add add protocol directory

--- protocol/packet.py
+++ protocol/packet.py
@@ -12,14 +12,22 @@
 """
 
 
-def func():
-    pass
+class Packet(object):
+    def __init__(self, msg_type=None, payload=None):
+        self.__msg_type = msg_type
+        self.__payload = payload
 
+    def parse_packet(self, data):
+        """
 
-class Packet():
-    def __init__(self):
-        pass
+        :param data:
+        :return:
+        """
+        msg_array = data.split("|")
+        self.__msg_type = msg_array[0]
 
+    def get_message_type(self):
+        return self.__msg_type
 
 if __name__ == '__main__':
     pass
